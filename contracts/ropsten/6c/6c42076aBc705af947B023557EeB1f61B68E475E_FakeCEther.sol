c6dc09121aa89d984a2342ae5abd17d009c46233
GithubAction/updateContractsEtherscanWebRopsten <pull@users.noreply.github.com>
2021-03-04 11:19:50 +0000

    GithubAction - updateContractsEtherscanWebRopsten (ropsten) - autocommit

---
+++ contracts/ropsten/6c/6c42076aBc705af947B023557EeB1f61B68E475E_FakeCEther.sol
@@ -0,0 +1,21 @@
+pragma solidity ^0.5.16;
+
+import "./src/CEther.sol";
+
+contract FakeCEther is CEther {
+    constructor(
+      ComptrollerInterface _comptroller,
+      InterestRateModel _interestRateModel
+    )
+    CEther(
+      _comptroller,
+      _interestRateModel,
+      200000000 * (10 ** 18),
+      "Fake Compound Ether",
+      "cETH",
+      8,
+      msg.sender
+    )
+    public
+    {}
+}
