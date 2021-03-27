8bce56ca00a3f6621fa418cf4605d58b4858aa25
GithubAction/updateContractsEtherscanWebRopsten <pull@users.noreply.github.com>
2021-03-27 23:07:46 +0000

    GithubAction - updateContractsEtherscanWebRopsten (ropsten) - autocommit

---
+++ contracts/ropsten/f4/f471DC3D2D45E01AE10fF88b7d5cF29687B14674_KittyToken.sol
@@ -0,0 +1,93 @@
+pragma solidity >=0.4.22 <0.9.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721Full.sol";
+import "@openzeppelin/contracts/ownership/Ownable.sol";
+import "@openzeppelin/contracts/lifecycle/Pausable.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+//import "@openzeppelin/contracts/utils/Counters.sol";
+
+// NFT Kitty token
+// Stores 1 value for every token: attribute
+
+contract KittyToken is ERC721Full, Ownable, Pausable, ReentrancyGuard {
+  
+  string public constant name = "SpaceX Kat Token";
+  string public constant symbol = "SXKT";
+  using Counters for Counters.Counter;
+  Counters.Counter private tokenId;
+
+  struct Kitty {
+    uint256 kittyTokenId;
+    string birthday;
+    string gender;
+    uint256 generation;
+    uint256 motherId;
+    uint256 fatherId;
+    string kittyType;
+    string imageURL;
+  }
+  
+  //Kitty[] public kitties;
+  mapping(uint256 => Kitty) public kittyDictionary;
+
+  constructor(
+  )
+    ERC721Full(name, symbol)
+    public
+  {}
+
+  // Return name, gender, attribute of a kitty token
+  function getKitty( uint256 inputTokenId ) 
+    public view returns(uint256 kittyTokenId,
+      string memory birthday,
+      string memory gender,
+      uint256 generation,
+      uint256 motherId,
+      uint256 fatherId,
+      string memory kittyType, 
+      string memory imageURL) {
+
+    Kitty memory _kitty = kittyDictionary[inputTokenId];
+
+    kittyTokenId = _kitty.kittyTokenId;
+    birthday = _kitty.birthday;
+    gender = _kitty.gender;
+    generation = _kitty.generation;
+    motherId = _kitty.motherId;
+    fatherId = _kitty.fatherId;
+    kittyType = _kitty.kittyType;
+    imageURL = _kitty.imageURL;
+  }
+	
+  // Create a new Kitty token with params: name, gender, attribute
+  function mint(address _to, 
+    uint256 _kittyTokenId,
+    string memory _birthday,
+    string memory _gender,
+    uint256 _generation,
+    uint256 _motherId,
+    uint256 _fatherId,
+    string memory _kittyType,
+    string memory _imageURL,
+    string memory _tokenURI) public payable onlyOwner {
+
+    //uint256 kittyTokenId = tokenId.current();
+    
+    Kitty memory _kitty = Kitty({ 
+      kittyTokenId: _kittyTokenId,
+      birthday : _birthday, 
+      gender: _gender,
+      generation : _generation,
+      motherId : _motherId,
+      fatherId : _fatherId,
+      kittyType : _kittyType,
+      imageURL : _imageURL
+    });
+
+    //kitties.push(_kitty);
+    kittyDictionary[_kittyTokenId] = _kitty;
+    _mint(_to, _kittyTokenId);
+    _setTokenURI(_kittyTokenId, _tokenURI);
+    //tokenId.increment();
+  }
+}
