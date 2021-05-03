2544c5a351ae7d4be250b0935ec4044a73998f32
GithubAction/updateContractsEtherscanWebRopsten <pull@users.noreply.github.com>
2021-05-03 11:20:26 +0000

    GithubAction - updateContractsEtherscanWebRopsten (ropsten) - autocommit

---
+++ contracts/ropsten/7c/7cf1DFEC2A631d6C3BcAEaeE65949d08eBEBa725_ReferralPayments.sol
@@ -0,0 +1,194 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.4;
+
+import "@openzeppelin/contracts/utils/Address.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+
+contract ReferralPayments is ReentrancyGuard {
+    using Address for address;
+    using SafeMath for uint256;
+    using SafeERC20 for IERC20;
+
+    address payable public governor;
+
+    mapping(address => uint256) public lastClaimedAt;
+
+    mapping(address => uint256) public totalClaimed;
+    mapping(address => uint256) public totalUnclaimed;
+    mapping(string => mapping(address => uint256)) public claimedAmounts;
+    mapping(string => mapping(address => uint256)) public claimableAmounts;
+
+    mapping(address => uint256) public totalRevoked;
+
+    address[] public tokens;
+
+    mapping(address => string) private tokensSymbols;
+    mapping(string => address) private tokensBySymbol;
+
+    struct SupportedToken {
+        string symbol;
+        address location;
+    }
+
+    uint256 revokingPeriodInSecs = 7889400; // 3 months
+
+    event Claim(address token, address owner, uint256 amount);
+    event AddClaimableAmount(address token, address owner, uint256 amount);
+    event RevokeClaimableAmount(address token, address owner, uint256 amount);
+
+    constructor(SupportedToken[] memory supportedTokens) {
+        governor = payable(msg.sender);
+
+        for (uint256 counter = 0; counter < supportedTokens.length; counter++) {
+            SupportedToken memory token = supportedTokens[counter];
+
+            _addToken(token.location, token.symbol);
+        }
+    }
+
+    function setGovernor(address payable newGovernor) external {
+        require(msg.sender == governor, "ReferralPayments: !governor");
+
+        governor = newGovernor;
+    }
+
+    function addToken(address token, string memory symbol) external {
+        require(msg.sender == governor, "ReferralPayments: !governor");
+
+        _addToken(token, symbol);
+    }
+
+    function _addToken(address token, string memory symbol) internal {
+        require(
+            bytes(tokensSymbols[token]).length == 0,
+            "ReferralPayments: token already exists"
+        );
+
+        tokensSymbols[token] = symbol;
+        tokensBySymbol[symbol] = token;
+
+        tokens.push(token);
+    }
+
+    function addClaimableAmount(
+        string memory token,
+        address owner,
+        uint256 amount
+    ) external {
+        require(msg.sender == governor, "ReferralPayments: !governor");
+
+        address tokenAddress = tokensBySymbol[token];
+        uint256 currentAmount = claimableAmounts[token][owner];
+
+        claimableAmounts[token][owner] = currentAmount.add(amount);
+        totalUnclaimed[tokenAddress] = totalUnclaimed[tokenAddress].add(amount);
+
+        emit AddClaimableAmount(tokenAddress, owner, amount);
+    }
+
+    function transferOut(address token, uint256 amount) external {
+        require(msg.sender == governor, "ReferralPayments: !governor");
+
+        IERC20(token).safeTransfer(governor, amount);
+    }
+
+    function revokeUnclaimedAmounts(address[] memory owners) external {
+        require(msg.sender == governor, "ReferralPayments: !governor");
+
+        for (uint256 counter = 0; counter < owners.length; counter++) {
+            address owner = owners[counter];
+            uint256 lastClaimedDate = lastClaimedAt[owner];
+
+            if (block.timestamp - lastClaimedDate <= revokingPeriodInSecs) {
+                continue;
+            }
+
+            for (uint256 i = 0; i < tokens.length; i++) {
+                address tokenAddress = tokens[i];
+                string memory tokenSymbol = tokensSymbols[tokenAddress];
+
+                uint256 amountToRevoke = claimableAmounts[tokenSymbol][owner];
+
+                if (amountToRevoke == 0) {
+                    continue;
+                }
+
+                _revokeUnclaimedAmount(
+                    owner,
+                    SupportedToken(tokenSymbol, tokenAddress),
+                    amountToRevoke
+                );
+            }
+        }
+    }
+
+    function _revokeUnclaimedAmount(
+        address owner,
+        SupportedToken memory supportedToken,
+        uint256 amountToRevoke
+    ) internal {
+        claimableAmounts[supportedToken.symbol][owner] = 0;
+        claimedAmounts[supportedToken.symbol][owner] = claimedAmounts[
+            supportedToken.symbol
+        ][owner]
+            .add(amountToRevoke);
+
+        totalRevoked[supportedToken.location] = totalRevoked[
+            supportedToken.location
+        ]
+            .add(amountToRevoke);
+        totalUnclaimed[supportedToken.location] = totalUnclaimed[
+            supportedToken.location
+        ]
+            .sub(amountToRevoke);
+
+        emit RevokeClaimableAmount(
+            supportedToken.location,
+            owner,
+            amountToRevoke
+        );
+    }
+
+    function claim(address owner) external nonReentrant {
+        for (uint256 counter = 0; counter < tokens.length; counter++) {
+            address tokenAddress = tokens[counter];
+            string memory tokenSymbol = tokensSymbols[tokenAddress];
+
+            _claim(owner, SupportedToken(tokenSymbol, tokenAddress));
+        }
+    }
+
+    function _claim(address owner, SupportedToken memory supportedToken)
+        internal
+    {
+        uint256 amountToClaim = claimableAmounts[supportedToken.symbol][owner];
+
+        if (amountToClaim == 0) {
+            return;
+        }
+
+        claimableAmounts[supportedToken.symbol][owner] = 0;
+        claimedAmounts[supportedToken.symbol][owner] = claimedAmounts[
+            supportedToken.symbol
+        ][owner]
+            .add(amountToClaim);
+
+        IERC20(supportedToken.location).safeTransfer(owner, amountToClaim);
+
+        totalClaimed[supportedToken.location] = totalClaimed[
+            supportedToken.location
+        ]
+            .add(amountToClaim);
+        totalUnclaimed[supportedToken.location] = totalUnclaimed[
+            supportedToken.location
+        ]
+            .sub(amountToClaim);
+
+        emit Claim(supportedToken.location, owner, amountToClaim);
+
+        lastClaimedAt[owner] = block.timestamp;
+    }
+}
---
+++ contracts/ropsten/a2/a2Fe9011e2DA870f56B6Ba520Ad4Eab1F6AD6b5f_MovieVoting.sol
@@ -0,0 +1,478 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.6.2;
+
+import "@openzeppelin/contracts/math/SafeMath.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
+import "./matic/BasicMetaTransaction.sol";
+import "./interfaces/IMogulSmartWallet.sol";
+import "./interfaces/IMovieVotingMasterChef.sol";
+import "hardhat/console.sol";
+
+contract MovieVoting is BasicMetaTransaction, AccessControl {
+    using SafeMath for uint256;
+
+    bytes32 public constant ROLE_ADMIN = keccak256("ROLE_ADMIN");
+
+    IERC20 public stars;
+    IERC1155 public mglMovie;
+    IMovieVotingMasterChef movieVotingMasterChef;
+    uint256 public constant MAX_MOVIES = 5;
+
+    enum VotingRoundState {Active, Paused, Canceled, Executed}
+
+    struct VotingRound {
+        uint256[MAX_MOVIES] movieIds;
+        uint256 startVoteBlockNum;
+        uint256 endVoteBlockNum;
+        uint256 starsRewards;
+        VotingRoundState votingRoundState;
+        // mapping variables: movieId
+        mapping(uint256 => uint256) votes;
+        // mapping variables: userAddress
+        mapping(address => bool) rewardsClaimed;
+        // mapping variables: userAddress, movieId
+        mapping(address => mapping(uint256 => uint256)) totalStarsEntered;
+    }
+
+    VotingRound[] public votingRounds;
+
+    event VotingRoundCreated(
+        uint256[MAX_MOVIES] movieIds,
+        uint256 startVoteBlockNum,
+        uint256 endVoteBlockNum,
+        uint256 starsRewards,
+        uint256 votingRound
+    );
+    event VotingRoundPaused(uint256 roundId);
+    event VotingRoundUnpaused(uint256 roundId);
+    event VotingRoundCanceled(uint256 roundId);
+    event VotingRoundExecuted(uint256 roundId);
+
+    event Voted(
+        address voter,
+        uint256 roundId,
+        uint256 movieId,
+        uint256 starsAmountMantissa,
+        uint256 quadraticVoteScore
+    );
+    event Unvoted(
+        address voter,
+        uint256 roundId,
+        uint256 movieId,
+        uint256 starsAmountMantissa,
+        uint256 quadraticVoteScore
+    );
+
+    modifier onlyAdmin {
+        require(hasRole(ROLE_ADMIN, msgSender()), "Sender is not admin");
+        _;
+    }
+
+    modifier votingRoundMustExist(uint256 roundId) {
+        require(
+            roundId < votingRounds.length,
+            "Voting Round id does not exist yet"
+        );
+        _;
+    }
+
+    constructor(
+        address _admin,
+        address _stars,
+        address _mglMovie,
+        address _movieVotingMasterChef
+    ) public {
+        _setupRole(ROLE_ADMIN, _admin);
+        _setRoleAdmin(ROLE_ADMIN, ROLE_ADMIN);
+
+        stars = IERC20(_stars);
+        mglMovie = IERC1155(_mglMovie);
+        movieVotingMasterChef = IMovieVotingMasterChef(_movieVotingMasterChef);
+        // Note: uint256(-1) is max number
+        stars.approve(_movieVotingMasterChef, uint256(-1));
+    }
+
+    function getMovieIds(uint256 votingRoundId)
+        public
+        view
+        returns (uint256[MAX_MOVIES] memory)
+    {
+        return votingRounds[votingRoundId].movieIds;
+    }
+
+    function getMovieVotes(uint256 votingRoundId)
+        public
+        view
+        returns (uint256[MAX_MOVIES] memory)
+    {
+        VotingRound storage votingRound = votingRounds[votingRoundId];
+        uint256[MAX_MOVIES] memory votes;
+        for (uint256 i; i < MAX_MOVIES; i++) {
+            votes[i] = (votingRound.votes[votingRound.movieIds[i]]);
+        }
+        return votes;
+    }
+
+    function getVotingRound(uint256 votingRoundId)
+        public
+        view
+        returns (
+            uint256[MAX_MOVIES] memory,
+            uint256[MAX_MOVIES] memory,
+            uint256,
+            uint256,
+            uint256,
+            VotingRoundState
+        )
+    {
+        VotingRound storage votingRound = votingRounds[votingRoundId];
+        uint256[MAX_MOVIES] memory movieIds = votingRound.movieIds;
+        uint256[MAX_MOVIES] memory votes;
+
+        for (uint256 i; i < MAX_MOVIES; i++) {
+            votes[i] = (votingRound.votes[votingRound.movieIds[i]]);
+        }
+        return (
+            movieIds,
+            votes,
+            votingRound.startVoteBlockNum,
+            votingRound.endVoteBlockNum,
+            votingRound.starsRewards,
+            votingRound.votingRoundState
+        );
+    }
+
+    function getUserMovieTotalStarsEntered(
+        address userAddress,
+        uint256 movieId,
+        uint256 votingRoundId
+    ) public view returns (uint256) {
+        uint256 userMovieTotalStarsEntered =
+            votingRounds[votingRoundId].totalStarsEntered[userAddress][movieId];
+        return userMovieTotalStarsEntered;
+    }
+
+    function didUserClaimRewards(
+        address userAddress,
+        uint256 movieId,
+        uint256 votingRoundId
+    ) public view returns (bool) {
+        bool didUserClaimRewards =
+            votingRounds[votingRoundId].rewardsClaimed[userAddress];
+        return didUserClaimRewards;
+    }
+
+    function createNewVotingRound(
+        uint256[MAX_MOVIES] memory movieIds,
+        uint256 startVoteBlockNum,
+        uint256 endVoteBlockNum,
+        uint256 starsRewards
+    ) public onlyAdmin {
+        require(
+            startVoteBlockNum < endVoteBlockNum,
+            "Start block must be less than end block"
+        );
+
+        VotingRound memory votingRound;
+        votingRound.movieIds = movieIds;
+        votingRound.startVoteBlockNum = startVoteBlockNum;
+        votingRound.endVoteBlockNum = endVoteBlockNum;
+        votingRound.starsRewards = starsRewards;
+        votingRound.votingRoundState = VotingRoundState.Active;
+
+        votingRounds.push(votingRound);
+
+        stars.transferFrom(msgSender(), address(this), starsRewards);
+
+        // transfer stars for rewards
+        movieVotingMasterChef.add(
+            startVoteBlockNum,
+            endVoteBlockNum,
+            starsRewards,
+            false
+        );
+
+        emit VotingRoundCreated(
+            movieIds,
+            startVoteBlockNum,
+            endVoteBlockNum,
+            starsRewards,
+            votingRounds.length
+        );
+    }
+
+    function pauseVotingRound(uint256 roundId)
+        public
+        onlyAdmin
+        votingRoundMustExist(roundId)
+    {
+        VotingRound storage votingRound = votingRounds[roundId];
+
+        require(
+            votingRound.votingRoundState == VotingRoundState.Active,
+            "Only active voting rounds can be paused"
+        );
+        require(
+            votingRound.endVoteBlockNum >= block.number,
+            "Voting Round has already concluded"
+        );
+        votingRound.votingRoundState = VotingRoundState.Paused;
+
+        emit VotingRoundPaused(roundId);
+    }
+
+    function unpauseVotingRound(uint256 roundId)
+        public
+        onlyAdmin
+        votingRoundMustExist(roundId)
+    {
+        VotingRound storage votingRound = votingRounds[roundId];
+
+        require(
+            votingRound.votingRoundState == VotingRoundState.Paused,
+            "Only paused voting rounds can be unpaused"
+        );
+        votingRound.votingRoundState = VotingRoundState.Active;
+
+        emit VotingRoundUnpaused(roundId);
+    }
+
+    function cancelVotingRound(uint256 roundId)
+        public
+        onlyAdmin
+        votingRoundMustExist(roundId)
+    {
+        VotingRound storage votingRound = votingRounds[roundId];
+
+        require(
+            votingRound.votingRoundState == VotingRoundState.Active ||
+                votingRound.votingRoundState == VotingRoundState.Paused,
+            "Only active or paused voting rounds can be cancelled"
+        );
+        require(
+            block.number <= votingRound.endVoteBlockNum,
+            "Voting Round has already concluded"
+        );
+        votingRound.votingRoundState = VotingRoundState.Canceled;
+
+        emit VotingRoundCanceled(roundId);
+    }
+
+    function executeVotingRound(uint256 roundId)
+        public
+        onlyAdmin
+        votingRoundMustExist(roundId)
+    {
+        VotingRound storage votingRound = votingRounds[roundId];
+
+        require(
+            votingRound.votingRoundState == VotingRoundState.Active,
+            "Only active voting rounds can be executed"
+        );
+        require(
+            votingRound.endVoteBlockNum < block.number,
+            "Voting round has not ended"
+        );
+        votingRound.votingRoundState = VotingRoundState.Executed;
+
+        emit VotingRoundExecuted(roundId);
+    }
+
+    function totalVotingRounds() public view returns (uint256) {
+        return votingRounds.length;
+    }
+
+    function _verifySmartWalletOwner(
+        bool isMogulSmartWallet,
+        address mogulSmartWallet,
+        address msgSender
+    ) internal returns (address) {
+        if (isMogulSmartWallet == true) {
+            require(
+                msgSender == IMogulSmartWallet(mogulSmartWallet).owner(),
+                "Invalid Mogul Smart Wallet Owner"
+            );
+            return mogulSmartWallet;
+        } else {
+            return msgSender;
+        }
+    }
+
+    function voteForMovie(
+        uint256 roundId,
+        uint256 movieId,
+        uint256 starsAmountMantissa,
+        bool isMogulSmartWallet,
+        address mogulSmartWalletAddress
+    ) public votingRoundMustExist(roundId) {
+        require(
+            starsAmountMantissa >= 1 ether,
+            "Must deposit at least 1 Stars token"
+        );
+
+        address _msgSender =
+            _verifySmartWalletOwner(
+                isMogulSmartWallet,
+                mogulSmartWalletAddress,
+                msgSender()
+            );
+
+        VotingRound storage votingRound = votingRounds[roundId];
+
+        uint256[MAX_MOVIES] memory movieIds = votingRound.movieIds;
+        require(
+            movieId == movieIds[0] ||
+                movieId == movieIds[1] ||
+                movieId == movieIds[2] ||
+                movieId == movieIds[3] ||
+                movieId == movieIds[4],
+            "Movie Id is not in voting round"
+        );
+
+        require(
+            votingRound.votingRoundState == VotingRoundState.Active,
+            "Can only vote in active rounds"
+        );
+
+        require(
+            votingRound.startVoteBlockNum <= block.number &&
+                block.number <= votingRound.endVoteBlockNum,
+            "Voting round has not started or has ended"
+        );
+
+        votingRound.totalStarsEntered[_msgSender][movieId] = votingRound
+            .totalStarsEntered[_msgSender][movieId]
+            .add(starsAmountMantissa);
+
+        uint256 updatedUserTotalStarsEntered =
+            votingRound.totalStarsEntered[_msgSender][movieId];
+
+        uint256 quadraticVoteScore =
+            updatedUserTotalStarsEntered
+                .mul(updatedUserTotalStarsEntered)
+                .div(1 ether)
+                .div(1 ether);
+
+        votingRound.votes[movieId] = votingRound.votes[movieId].add(
+            quadraticVoteScore
+        );
+
+        movieVotingMasterChef.deposit(roundId, starsAmountMantissa, _msgSender);
+
+        emit Voted(
+            _msgSender,
+            roundId,
+            movieId,
+            starsAmountMantissa,
+            quadraticVoteScore
+        );
+    }
+
+    function removeVoteForMovie(
+        uint256 roundId,
+        uint256 movieId,
+        uint256 starsAmountMantissa,
+        bool isMogulSmartWallet,
+        address mogulSmartWalletAddress
+    ) public votingRoundMustExist(roundId) {
+        require(starsAmountMantissa > 0, "Cannot remove 0 votes");
+
+        address _msgSender =
+            _verifySmartWalletOwner(
+                isMogulSmartWallet,
+                mogulSmartWalletAddress,
+                msgSender()
+            );
+
+        VotingRound storage votingRound = votingRounds[roundId];
+
+        uint256[MAX_MOVIES] memory movieIds = votingRound.movieIds;
+        require(
+            movieId == movieIds[0] ||
+                movieId == movieIds[1] ||
+                movieId == movieIds[2] ||
+                movieId == movieIds[3] ||
+                movieId == movieIds[4],
+            "Movie Id is not in voting round"
+        );
+        require(
+            starsAmountMantissa <=
+                votingRound.totalStarsEntered[_msgSender][movieId],
+            "Not enough Stars to remove"
+        );
+        require(
+            votingRound.votingRoundState == VotingRoundState.Active,
+            "Can only remove vote in active rounds"
+        );
+
+        require(
+            votingRound.startVoteBlockNum <= block.number &&
+                block.number <= votingRound.endVoteBlockNum,
+            "Voting round has not started or ended"
+        );
+
+        votingRound.totalStarsEntered[_msgSender][movieId] = votingRound
+            .totalStarsEntered[_msgSender][movieId]
+            .sub(starsAmountMantissa);
+
+        uint256 updatedUserTotalStarsEntered =
+            votingRound.totalStarsEntered[_msgSender][movieId];
+
+        movieVotingMasterChef.emergencyWithdraw(roundId, _msgSender);
+
+        uint256 quadraticVoteScore =
+            updatedUserTotalStarsEntered
+                .mul(updatedUserTotalStarsEntered)
+                .div(1 ether)
+                .div(1 ether);
+
+        votingRound.votes[movieId] = quadraticVoteScore;
+
+        //stars.transfer(_msgSender, starsAmountMantissa);
+
+        emit Unvoted(
+            _msgSender,
+            roundId,
+            movieId,
+            starsAmountMantissa,
+            quadraticVoteScore
+        );
+    }
+
+    function calculateStarsRewards(
+        address userAddress,
+        uint256 roundId,
+        uint256 movieId
+    ) public view votingRoundMustExist(roundId) returns (uint256) {
+        VotingRound storage votingRound = votingRounds[roundId];
+
+        return movieVotingMasterChef.pendingStars(roundId, userAddress);
+    }
+
+    function withdrawAndClaimStarsRewards(
+        uint256 roundId,
+        uint256 movieId,
+        bool isMogulSmartWallet,
+        address mogulSmartWalletAddress
+    ) public votingRoundMustExist(roundId) {
+        address _msgSender =
+            _verifySmartWalletOwner(
+                isMogulSmartWallet,
+                mogulSmartWalletAddress,
+                msgSender()
+            );
+
+        VotingRound storage votingRound = votingRounds[roundId];
+
+        require(
+            votingRound.rewardsClaimed[_msgSender] == false,
+            "Rewards have already been claimed"
+        );
+
+        votingRound.rewardsClaimed[_msgSender] = true;
+
+        movieVotingMasterChef.withdraw(roundId, _msgSender);
+    }
+}
