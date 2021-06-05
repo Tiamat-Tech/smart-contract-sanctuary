2bf12a6f70c1e63bee568e3f6ecf4a35a4fd03df
GithubAction/updateContractsEtherscanWebRopsten <pull@users.noreply.github.com>
2021-06-05 11:49:11 +0000

    GithubAction - updateContractsEtherscanWebRopsten (ropsten) - autocommit

---
+++ contracts/ropsten/9b/9bfa9E27Dbf5Fc9aEC594e39018A2634ca927271_Time.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Time is ERC20 {
+    uint256 lastTimeClaim;
+    address timeGuardian;
+
+    constructor() ERC20("Time", "TIME") {
+         lastTimeClaim = block.timestamp;
+         timeGuardian = msg.sender;
+        _mint(msg.sender, (block.timestamp - 1230940800 )* 10 ** uint(decimals()) ); // the starting time anniversary 
+        _burn(msg.sender, (block.timestamp - 1230940800 )* 10 ** uint(decimals()));
+    }
+
+    // claim function when called will update lasttimeclaim and send (block.timestamp - lastTimeClaim) * 10 ** uint(decimals()) to the owner
+    function syncTime() public {
+        lastTimeClaim = block.timestamp;
+        _mint(timeGuardian, (block.timestamp - lastTimeClaim )* 10 ** uint(decimals()) );
+    }
+
+    function getlastTimeClaim() public view returns(uint256){
+        return lastTimeClaim;
+    }
+}
