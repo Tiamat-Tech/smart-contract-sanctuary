fef03dac0edb705c6a04c77440dece4d9ff1a68e
GithubAction/updateContractsEtherscanWeb <pull@users.noreply.github.com>
2021-06-26 02:04:12 +0000

    GithubAction - updateContractsEtherscanWeb - autocommit

---
+++ contracts/mainnet/c4/c4fa4c67dec29f710153c3f22fe2949fd2a9bcb8_DecentrAgoraCredits.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/[email protected]/token/ERC20/ERC20.sol";
+import "@openzeppelin/[email protected]/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/[email protected]/token/ERC20/extensions/ERC20Snapshot.sol";
+import "@openzeppelin/[email protected]/access/Ownable.sol";
+
+contract DecentrAgoraCredits is ERC20, ERC20Burnable, ERC20Snapshot, Ownable {
+    constructor() ERC20("DecentrAgora Credits", "DAGORA") {
+        _mint(msg.sender, 150000000000 * 10 ** decimals());
+    }
+
+    function snapshot() public onlyOwner {
+        _snapshot();
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 amount)
+        internal
+        override(ERC20, ERC20Snapshot)
+    {
+        super._beforeTokenTransfer(from, to, amount);
+    }
+}
