b7fea7149a2412dec9df80067577630de1453341
GithubAction/updateContractsEtherscanWeb <pull@users.noreply.github.com>
2021-07-14 01:56:15 +0000

    GithubAction - updateContractsEtherscanWeb - autocommit

---
+++ contracts/mainnet/3c/3C7503Bd7d66c15e13CBC42953436e598d37C792_SLIME.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+/// @title: Slimesunday
+/// @author: manifold.xyz
+
+import "./ERC721Creator.sol";
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//                                                                                                                               //
+//                                                                                                                               //
+//      [email protected]@@@@@=  [email protected]@@@    [email protected]@@@[email protected]@@@@= [email protected]@@@[email protected]@@@@@@@- [email protected]@@@@@@-  @@@@@ [email protected]@@@[email protected]@@@  @@@@[email protected]@@@@@@=-   [email protected]@@@@@[email protected]@@@= [email protected]@@=    //
+//      [email protected]@@@@@@@@= @@@@@    @@@@@[email protected]@@@@= @@@@@[email protected]@@@@@@@ [email protected]@@@@@@@@[email protected]@@@@ @@@@@[email protected]@@@@- @@@@[email protected]@@@@@@@@@-  @@@@@@@ @@@@= @@@@-   //
+//      @@@@@[email protected]@@@@[email protected]@@@@   [email protected]@@@[email protected]@@@@[email protected]@@@@[email protected]@@@@@@@[email protected]@@@@[email protected]@@@[email protected]@@@[email protected]@@@@[email protected]@@@@[email protected]@@[email protected]@@@@@@@@@=  @@@@@@@ @@@@[email protected]@@=    //
+//      @@@@@ @@@@[email protected]@@@=   [email protected]@@@[email protected]@@@@[email protected]@@@@@[email protected]@@@@-   [email protected]@@@[email protected]@@@[email protected]@@@[email protected]@@@[email protected]@@@@[email protected]@@[email protected]@@@[email protected]@@@= [email protected]@@@@@@ [email protected]@@@@@@@     //
+//      @@@@@@-    [email protected]@@@-   @@@@@[email protected]@@@@@[email protected]@@@@@[email protected]@@@@    [email protected]@@@@=     @@@@@ [email protected]@@@[email protected]@@@@[email protected]@@@[email protected]@@@@ @@@@@- @@@@@@@@ [email protected]@@@@@@-     //
+//      [email protected]@@@@@-   @@@@@    @@@@@[email protected]@@@@@@@@@@@[email protected]@@@@@@=  @@@@@@@   [email protected]@@@@ @@@@@[email protected]@@@@@@@@@@[email protected]@@@@ @@@@@ [email protected]@@@@@@@ [email protected]@@@@@=      //
+//       [email protected]@@@@@=  @@@@@   [email protected]@@@[email protected]@@@@@@@@@@@[email protected]@@@@@@-   @@@@@@@- [email protected]@@@= @@@@@[email protected]@@@@@@@@@[email protected]@@@[email protected]@@@@ @@@@[email protected]@@@  @@@@@@       //
+//        [email protected]@@@@@[email protected]@@@=   [email protected]@@@[email protected]@@@@@@@@@@@[email protected]@@@@@@@     [email protected]@@@@@ [email protected]@@@[email protected]@@@[email protected]@@@@@@@@@[email protected]@@@[email protected]@@@[email protected]@@[email protected]@@@  @@@@@=       //
+//    [email protected]@@@@[email protected]@@@-   @@@@@[email protected]@@@[email protected]@@[email protected]@@@[email protected]@@@=    [email protected]@@@@[email protected]@@@@ [email protected]@@@[email protected]@@[email protected]@@@@[email protected]@@@@ @@@@@[email protected]@@@[email protected]@@@@  @@@@@        //
+//    @@@@@ @@@@@[email protected]@@@@--- @@@@@[email protected]@@@[email protected]@@[email protected]@@@[email protected]@@@[email protected]@@@= @@@@@[email protected]@@@@[email protected]@@@@[email protected]@@@ [email protected]@@@@[email protected]@@@@ @@@@@[email protected]@@@@@@@@@  @@@@@        //
+//    @@@@@@@@@@@[email protected]@@@@@@@[email protected]@@@[email protected]@@[email protected]@[email protected]@@[email protected]@@@@@@@[email protected]@@@@@@@@@@[email protected]@@@@@@@@@@[email protected]@@@ [email protected]@@@@[email protected]@@@@@@@@@[email protected]@@@@@@@@@ [email protected]@@@=        //
+//    [email protected]@@@@@@@@[email protected]@@@@@@@[email protected]@@@[email protected]@@[email protected]@ @@@@[email protected]@@@@@@@@ @@@@@@@@@@  @@@@@@@@@@ [email protected]@@= [email protected]@@@[email protected]@@@@@@@@@[email protected]@@@[email protected]@@@@ [email protected]@@@-        //
+//     [email protected]@@@@@=- [email protected]@@@@@@[email protected]@@@[email protected]@@ [email protected] @@@@[email protected]@@@@@@@=  [email protected]@@@@@=    [email protected]@@@@@=  [email protected]@@-  @@@@[email protected]@@@@@@@= [email protected]@@=  @@@@= [email protected]@@@         //
+//                                                                                                                               //
+//                                                                                                                               //
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+contract SLIME is ERC721Creator {
+    constructor() ERC721Creator("Slimesunday", "SLIME") {}
+}
