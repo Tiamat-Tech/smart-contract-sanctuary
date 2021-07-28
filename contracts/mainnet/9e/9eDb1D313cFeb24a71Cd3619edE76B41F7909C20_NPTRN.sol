b23fc285b4206a9660203ace36e7a19812ee1c9f
GithubAction/updateContractsEtherscanWeb <pull@users.noreply.github.com>
2021-07-28 02:04:24 +0000

    GithubAction - updateContractsEtherscanWeb - autocommit

---
+++ contracts/mainnet/9e/9eDb1D313cFeb24a71Cd3619edE76B41F7909C20_NPTRN.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+/// @title: NoPattern
+/// @author: manifold.xyz
+
+import "./ERC721Creator.sol";
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//                                                                                                                     //
+//                                                                                                                     //
+//           -+++*.      +*.=#.         -%%%%-:%+ -#@%                                                                 //
+//         .%@@@@@*.   [email protected]@@@@*         :@@@@@%@@##@@@@%+%:              ==     .*-                                     //
+//        .#@@@@@@@*. *@@@@@*          @@@@@@@@@@@@@@@@@+              [email protected]#    .%@=                                     //
+//        [email protected]@-:@@@@@[email protected]*#@@:           @@=#@@@@@@[email protected]@@.             :%@%.   *@@+                                      //
+//        -#:  @@@@@@-  :@%            @@. #@@@@*  :@@@              [email protected]@*    @@@-                                      //
+//             @@@@@@+:[email protected]%                #@@@@%- [email protected]@@              [email protected]@* .- @@@- -.                                   //
+//             @@%@@@@@@@@%        ..      #@@@@@@@@@@@             .#@@@#@@[email protected]@@%@@=    *@=   [email protected]#   :#@- .+*++*      //
+//           [email protected]@ %@@@*-*@#       [email protected]@+=* -#@@@@@+  [email protected]@@  [email protected]@%+%@@++:%@@@@@@@@@@@@@#-::*%@@@[email protected]@@@@@%#@@@@@#@@@@@*      //
+//          [email protected]@@@ -%@@#:[email protected]*  :++*@@@@@@%%@@@@@@@=  :@@@  *@@@@@@@@#%@@@@@%*@@@@@@#%@@@@@@@@@@@@@@@@@@@@@@@@@@@@%       //
+//          [email protected]@@@  #@@@[email protected]*  @@@@+#@@@% @@@@@@@@+::[email protected]@@  =%@@@@@@@.:-*@@#  [email protected]@@  -=%@@@@@@@@@@@@##*- @@@@*%@@@%       //
+//          [email protected]@@@@@@@@@@@@*   @@@:*@@@# ##+#@@@@@@@@@@@   [email protected]@@@@@@.  [email protected]@#    @@@    #@@*[email protected]@#:@@@*     @@@@.#@@@%       //
+//          :#[email protected]@###@@@@@@*   @@@:*@@@*    *@@@@#==*@@@   [email protected]@@@@@@.  [email protected]@#    @@@    #@@@@#:  @@@*     @@@@.#@@@%       //
+//            [email protected]%   :%@@@@*   @@@.*@@@+    %@@@@+  [email protected]@@ .*@%=#@@@@.  [email protected]@#    @@@    #@@%:    @@@*     %@@@.#@@@%       //
+//            :@@    %@@@@*   @@@ #@@@+  :#@@@@@@*-*@@@[email protected]@@*.*@@@@-  *@@%::  @@@..  #@@#.+## @@@%-:=  %@@@=%@@@@+      //
+//          [email protected]@@@+%@[email protected]@@@*  [email protected]@@*@@@@+ #@@@@@@@@@@@@@@#%@@@#@@@@@@:[email protected]@@@@% @@@@@@-#@@@@@@@=*@@@@@@# *@@@@@%@@@@*      //
+//        [email protected]@@@@@@@@# %@@@=.%@@@@@@@%- #@@@@@@@@@@@#:   *@@@@[email protected]@#: *@@@@=  %%@@#:.=##@@@=  =*%@@#:  +=#@@: [email protected]*.       //
+//       [email protected]@%**#@@@:   %@@-.+:[email protected]@%-   [email protected]@= %@@@@@@*      -+=.  =:     :=      ::      ::      ...      ..              //
+//       @@#.   -#-     .             :%=  %@@@@%.                                                                     //
+//       @@:                               %@@@@#                                                                      //
+//       @@-                               %@@@@*                                                                      //
+//       %@@#:                            *@@@@@*                                                                      //
+//        %@@@@@+                         @@==%@=                                                                      //
+//         .=**+.                         .                                                                            //
+//                                                                                                                     //
+//                                                                                                                     //
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+contract NPTRN is ERC721Creator {
+    constructor() ERC721Creator("NoPattern", "NPTRN") {}
+}
