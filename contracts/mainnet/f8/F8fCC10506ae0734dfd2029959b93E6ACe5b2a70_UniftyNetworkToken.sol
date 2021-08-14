9f28a7a355f7d11adc18d00bef53554596ca2954
GithubAction/updateContractsEtherscanWeb <pull@users.noreply.github.com>
2021-08-14 01:58:22 +0000

    GithubAction - updateContractsEtherscanWeb - autocommit

---
+++ contracts/mainnet/f8/F8fCC10506ae0734dfd2029959b93E6ACe5b2a70_UniftyNetworkToken.sol
@@ -0,0 +1,17 @@
+pragma solidity 0.8.4;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract UniftyNetworkToken is ERC20Capped, Ownable {
+    
+    constructor(uint256 _hard_cap) ERC20("Unifty Network Token", "UNT") ERC20Capped(_hard_cap) {
+        
+    }
+
+    /// @notice Creates _amount token to _to. Must only be called by the owner (UniftyGovernance).
+    function mint(address _to, uint256 _amount) public onlyOwner {
+        _mint(_to, _amount);
+    }
+}
