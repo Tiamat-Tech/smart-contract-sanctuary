3e4b7d647d85a9724612a8bdae0bfe831a713d2d
GithubAction/updateContractsEtherscanWeb <pull@users.noreply.github.com>
2021-09-21 13:28:59 +0000

    GithubAction - updateContractsEtherscanWeb - autocommit

---
+++ contracts/mainnet/7b/7b09c5C3069a7d10f27AB08AE564E96Df7e8AFc0_GRA.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+/// @title: Raf Grassetti
+/// @author: manifold.xyz
+
+import "@manifoldxyz/creator-core-solidity/contracts/ERC721Creator.sol";
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//                                                                                                                                                                                                 //
+//                                                                                                                                                                                                 //
+//       ,,,,,,,,,,,,,,,,,      ,,,,,,,,,,,,,,,,,            ,,            ,gggggggggggggggg.      ggggggggggggggggg    gggggggggggggggggg   ggggggggggggggggg-    gggggggggggggggggg      ,gg     //
+//      'PPPPPPPPPPPPPPPPP     'PPPPPPPPPPPPPPPPP           4MMM           """""""""""""""""`     """"""""""""""""""    `****************"   *****************     "****************"      "**     //
+//                                                                                                                                                                                                 //
+//       gg,         ggggg      ,gggggggggggggggg.        gg    ,g,        gggggggggggggggggg     4ggggggggggggggggg     ggggggggggggggggg          ,,,                   ,,,              ,,,     //
+//      ]@@@         *"]@@P     @@@[email protected]@"""       ,@@      %@g                    `'[email protected]@P                  ``@@@   @@@``            -          ]@@P                  ]@@              ]@@-    //
+//      ]@@@            @@P     @@K         %@@,       ,@@        ]@N                     ]@@P                    @@@   @@@                         ]@@P                  ]@@              ]@@-    //
+//      '@@@,,,,,,,,,,,[email protected]@P     @@K          ]@@p     /@@          ]@@     ,,,,,,,,,,,,,,[email protected]@@`    ,,,,,,,,,,,,,,,[email protected]@@   [email protected]@g,,,,,,,,,,,,,,          ]@@P                  ]@@              ]@@-    //
+//       -*NNNNNNNNNNNNNP"      NNP           ]NNP   4NP            "NN    ****************f"     "****************"     *****************          ]RR                   *RR              *RR     //
+//                                                                                                                                                                                                 //
+//                                                                                                                                                                                                 //
+//                                                                                                                                                                                                 //
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+contract GRA is ERC721Creator {
+    constructor() ERC721Creator("Raf Grassetti", "GRA") {}
+}
