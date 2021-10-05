f51939f7c915eeeb76866d70cae4b505d112ab26
GithubAction/updateContractsEtherscanWeb <pull@users.noreply.github.com>
2021-10-05 13:26:15 +0000

    GithubAction - updateContractsEtherscanWeb - autocommit

---
+++ contracts/mainnet/19/19314a0739700518f7c0f980dac9079a58680639_LOVEIS4LOVERS.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.4;
+
+/// @title LOVEIS4LOVERS
+/// @author jpegmint.xyz
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//                                                                                                                          //
+//     `@-`   -x/                                                                                                           //
+//      [email protected]@;[email protected]@+`                       `/x.                                                                                //
+//       [email protected]@%@+`                       [email protected]@+`                                                             .+`                //
+//       :@@[email protected],               .        %@#x`        .:            ;`         :;.     ,;        -:.`     :@%                 //
+//       [email protected]&@@,            [email protected]@%:.     ;@@@x;     -x%$`       `-%@@x-`    [email protected]@+%`.;[email protected]@@/.  `-+%@@@x%@@[email protected]%.                 //
+//       [email protected]@&@,       `.=x%&@[email protected]@$x+/.  :[email protected][email protected]@,   +%@@@;`    .+%@@@@[email protected]%/  .;%@@@@%+%@@@%@x :%@@@[email protected]@@@#@+`                  //
+//        @@#@,       /[email protected]@@+``%$%@@@@x  `@@#@x   `;%@@@,   [email protected]@#@%[email protected][email protected]%  `+#@@@%:``@@%:` [email protected]@@`   `%@@%-                    //
+//        @%@@;       [email protected]#@@:    ;@@[email protected]+  `@@@@x    `%@[email protected]@`  `@@#@`  -#@@/`  ;@@@%;`  ``    :@[email protected],   `[email protected]@+-.                   //
+//        @@[email protected]        `@@@@-    :@[email protected]@%   @@[email protected]     `@&#@   `@%@@; +%@-`    [email protected]#@@;         ;@@@$x-/%@#@@@%\-                 //
+//        @@@@        :@@[email protected]    ;@@@@;   @@#@;     ;@@#@   `@@@@/%+`       ;@@[email protected]:          ;[email protected]@@@@??.%@@@@+                 //
+//        %@[email protected],       /@$#@:    ;@@[email protected]%   #%@@;     :@@[email protected]    @%@$;/         ;@@#@:            [email protected]%:   `@[email protected]@+                 //
+//       ;@%@@,       [email protected]@%@+`   [email protected]#@@+  `@@?%;     ;@@@$`   @@#@;          /@%@@;            %[email protected]    `@@@@-   .x;. .:x.     //
+//       /%@@@#;,  . `[email protected]@[email protected]@%;[email protected]@@@o  :@@@@%%+-.;%@[email protected]@;  [email protected][email protected]@%-   [email protected]%   x%@@@@;/. ;.    .%@@[email protected]@%@;.  @#@%.%@[email protected]     //
+//      .+%%@@@@[email protected];  :x%@@&@@[email protected]#&@x+;  [email protected]@[email protected]@#@%%@@@%.   +%@@@#%x=-$%/  `-+%@@[email protected]%x+/`  `/[email protected]@%@@@&@@%@@%x.   ;@[email protected][email protected]@#+     //
+//        `+%@@@%/`     `-:%@@@@;%`        `/%%@@@%@-``     `.$%@@%;:`       .\@%@$-    .x#@@+%::[email protected]#@@x/.      ;[email protected]@@%;`     //
+//            %`             \%:              :%@`              `%              .%`    `:@`           `         `\?/`       //
+//                                                                                                                          //
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+import "@jpegmint/contracts/artists/ERC721Artist.sol";
+
+contract LOVEIS4LOVERS is ERC721Artist("LOVEIS4LOVERS", "LOVERS") {}
